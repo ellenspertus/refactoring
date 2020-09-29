4bd36b34bb9f25ee4001c3c057b42ecf25346032
Charles Thomas <chuckthemole@gmail.com>
2020-09-29 15:24:32 -0700

    Factor out getRentalCost()

---
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -0,0 +1,18 @@
+package bad.robot.refactoring.chapter1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RentalTest {
+	private static final Movie MOVIE_NEW = new Movie("Matrix", Movie.NEW_RELEASE);
+	private static final Movie MOVIE_CHILDRENS = new Movie("Children's Movie", Movie.NEW_RELEASE);
+	private static final Movie MOVIE_REGULAR = new Movie("Regular Movie", Movie.NEW_RELEASE);
+
+  @Test
+  void test() {
+	  void getAmount_CorrectValue_RegularMovie() {
+		  fail("Not yet implemented");
+	  }
+  }
+}
