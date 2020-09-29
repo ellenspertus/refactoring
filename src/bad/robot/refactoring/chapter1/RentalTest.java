4b16aff846bab65106c32c74a2828deb658b4ac8
Sarah O'Reilly <soreilly@mills.edu>
2020-09-29 16:11:48 -0700

    Remove last failing test

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,7 +1,6 @@
 package bad.robot.refactoring.chapter1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -23,11 +22,5 @@ class RentalTest {
         Rental childRental = new Rental(MOVIE_CHILDRENS, 3);
         assertEquals(cost, childRental.getRentalPrice(), "Wrong cost for " + days);
     }
-
-    void getRentalPrice_CorrectValue_RegularExtraDays() {
-        fail("Not yet implemented");
-
-    }
-
 
 }
