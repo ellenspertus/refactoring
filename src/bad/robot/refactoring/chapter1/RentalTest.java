f470786a43ac40d3f2b11fec84f895e2744a5d7e
Charles Thomas <chuckthemole@gmail.com>
2020-09-30 11:59:23 -0700

    Fix indents

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,21 +1,20 @@
 package bad.robot.refactoring.chapter1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class RentalTest {
-  private static final Movie MOVIE_NEW = new Movie("Matrix", Movie.NEW_RELEASE);
-  private static final Movie MOVIE_CHILDRENS = new Movie("Children's Movie", Movie.CHILDREN);
-  private static final Movie MOVIE_REGULAR = new Movie("Regular Movie", Movie.REGULAR);
+    private static final Movie MOVIE_NEW = new Movie("Matrix", Movie.NEW_RELEASE);
+    private static final Movie MOVIE_CHILDRENS = new Movie("Children's Movie", Movie.CHILDREN);
+    private static final Movie MOVIE_REGULAR = new Movie("Regular Movie", Movie.REGULAR);
 
-  // if (getDaysRented() > 2) amount += (getDaysRented() - 2) * 1.5;
+    // if (getDaysRented() > 2) amount += (getDaysRented() - 2) * 1.5;
 
-  @ParameterizedTest
-  @CsvSource({"2, 2", "3, 3.5", "4, 5"})
-  void getAmount_CorrectValue_RegularMovies(int days, double cost) {
-    Rental rental = new Rental(MOVIE_REGULAR, days);
-    assertEquals(cost, rental.getRentalPrice(), "Wrong cost!!!");
-  }
+    @ParameterizedTest
+    @CsvSource({"2, 2", "3, 3.5", "4, 5"})
+    void getAmount_CorrectValue_RegularMovies(int days, double cost) {
+        Rental rental = new Rental(MOVIE_REGULAR, days);
+        assertEquals(cost, rental.getRentalPrice(), "Wrong cost!!!");
+    }
 }
