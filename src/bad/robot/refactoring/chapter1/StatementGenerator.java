3e5843fab4f3e5d32c6f7096580933844ac12ae9
Ellen Spertus <spertus@mills.edu>
2020-09-30 17:20:50 -0700

    Add subclass TextStatementGenerator

--- src/bad/robot/refactoring/chapter1/StatementGenerator.java
+++ src/bad/robot/refactoring/chapter1/StatementGenerator.java
@@ -4,14 +4,17 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 public class StatementGenerator {
-    private static final String HEADER_TEMPLATE = "Rental Record for %1s\n";
-    private static final String RENTAL_TEMPLATE = "\t%1s\t%2s\n";
-    private static final String FOOTER_TEMPLATE =
-            "Amount owed is %1s\nYou earned %d frequent renter points\n";
+    private final String headerTemplate;
+    private final String rentalTemplate;
+    private final String footerTemplate;
 
-    private StatementGenerator() {}
+    protected StatementGenerator(String header, String rental, String footer) {
+        headerTemplate = header;
+        rentalTemplate = rental;
+        footerTemplate = footer;
+    }
 
-    public static String generateStatement(Customer customer) {
+    public String generateStatement(Customer customer) {
         StringBuilder sb = new StringBuilder();
         sb.append(generateHeader(customer));
         for (Rental rental : customer.getRentals()) {
@@ -21,21 +24,21 @@ public class StatementGenerator {
         return sb.toString();
     }
 
-    private static String generateHeader(Customer customer) {
-        return String.format(HEADER_TEMPLATE, customer.getName());
+    private String generateHeader(Customer customer) {
+        return String.format(headerTemplate, customer.getName());
     }
 
-    private static String generateRental(Rental rental) {
-        return String.format(RENTAL_TEMPLATE, rental.getMovie(),
+    private String generateRental(Rental rental) {
+        return String.format(rentalTemplate, rental.getMovie(),
                 formatMoney(rental.getRentalPrice()));
     }
 
-    private static String generateFooter(Customer customer) {
-        return String.format(FOOTER_TEMPLATE, formatMoney(customer.getTotalCharge()),
+    private String generateFooter(Customer customer) {
+        return String.format(footerTemplate, formatMoney(customer.getTotalCharge()),
                 customer.getFrequentRenterPoints());
     }
 
-    private static String formatMoney(BigDecimal bd) {
+    private String formatMoney(BigDecimal bd) {
         return NumberFormat.getCurrencyInstance().format(bd);
     }
 }
