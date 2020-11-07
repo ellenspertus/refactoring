130f83d00795f8b2c7493a6d24c4c8d5b29645ad
Zoe Abrams <zoej.abrams@gmail.com>
2020-11-07 12:52:18 -0800

    Add tests for new, regular and children's movies

--- src/bad/robot/refactoring/chapter1/Customer.java
+++ src/bad/robot/refactoring/chapter1/Customer.java
@@ -5,64 +5,44 @@ import java.util.List;
 
 public class Customer {
 
-    private String name;
-    private List<Rental> rentals = new ArrayList<Rental>();
+  private String name;
+  private List<Rental> rentals = new ArrayList<Rental>();
 
-    public Customer(String name) {
-        this.name = name;
+  public Customer(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void addRental(Rental rental) {
+    rentals.add(rental);
+  }
+
+  public String statement() {
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+
+    String result = "Rental record for " + getName() + "\n";
+    for (Rental rental : rentals) {
+      double amount = rental.getRentalPrice();
+
+      // add frequent renter points
+      frequentRenterPoints++;
+      // add bonus for a two day new release rental
+      if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+        frequentRenterPoints++;
+
+      // show figures for this rental
+      result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+
+      totalAmount += amount;
     }
 
-    public String getName() {
-        return name;
-    }
+    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
-    public void addRental(Rental rental) {
-        rentals.add(rental);
-    }
-
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-
-        String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentals) {
-            double amount = 0;
-            amount = getRentalPrice(rental);
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
-
-            totalAmount += amount;
-        }
-
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-
-        return result;
-    }
-
-    private double getRentalPrice(Rental rental) {
-      switch (rental.getMovie().getPriceCode()) {
-          case Movie.REGULAR:
-              amount += 2;
-              if (rental.getDaysRented() > 2)
-                  amount += (rental.getDaysRented() - 2) * 1.5;
-              break;
-          case Movie.NEW_RELEASE:
-              amount += rental.getDaysRented() * 3;
-              break;
-          case Movie.CHILDREN:
-              amount += 1.5;
-              if (rental.getDaysRented() > 3)
-                  amount += (rental.getDaysRented() - 3) * 1.5;
-              break;
-      }
-      return amount;
-    }
+    return result;
+  }
 }
--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,13 +1,29 @@
 package bad.robot.refactoring.chapter1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class RentalTest {
+  private static final Movie MOVIE_NEW = new Movie("New Movie", Movie.NEW_RELEASE);
+  private static final Movie MOVIE_CHILDRENS = new Movie("New Movie", Movie.CHILDREN);
+  private static final Movie MOVIE_REGULAR = new Movie("New Movie", Movie.REGULAR);
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void getAmount_CorrectValue_NewMovie() {
+    Rental rental = new Rental(MOVIE_NEW, 1); // simplest case, 1 day rented
+    assertEquals(3, rental.getRentalPrice());
+  }
+
+  @Test
+  void getAmount_CorrectValue_RegularMovie() {
+    Rental rental = new Rental(MOVIE_REGULAR, 1); // simplest case, 1 day rented
+    assertEquals(2, rental.getRentalPrice());
+  }
+
+  @Test
+  void getAmount_CorrectValue_ChildrensMovie() {
+    Rental rental = new Rental(MOVIE_CHILDRENS, 1); // simplest case, 1 day rented
+    assertEquals(1.5, rental.getRentalPrice());
   }
 
 }
