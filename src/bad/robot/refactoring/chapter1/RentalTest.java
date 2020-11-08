4463b967fd516479fcedb60d2f74a9bf1279b2c0
Zoe Abrams <zoej.abrams@gmail.com>
2020-11-07 16:26:19 -0800

    Add tests for all movie types
    Try named parameters in parametrized tests. All tests pass.

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,29 +1,33 @@
 package bad.robot.refactoring.chapter1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class RentalTest {
   private static final Movie MOVIE_NEW = new Movie("New Movie", Movie.NEW_RELEASE);
   private static final Movie MOVIE_CHILDRENS = new Movie("New Movie", Movie.CHILDREN);
   private static final Movie MOVIE_REGULAR = new Movie("New Movie", Movie.REGULAR);
 
-  @Test
-  void getAmount_CorrectValue_NewMovie() {
-    Rental rental = new Rental(MOVIE_NEW, 1); // simplest case, 1 day rented
-    assertEquals(3, rental.getRentalPrice());
+  @ParameterizedTest(name = "{index} => days={0}, cost={1}")
+  @CsvSource({"1,3", "2,6", "7,21"})
+  void getAmount_CorrectValue_NewReleaseMovie(int days, int cost) {
+    Rental rental = new Rental(MOVIE_NEW, days); // simplest case, 1 day rented
+    assertEquals(cost, rental.getRentalPrice());
   }
 
-  @Test
-  void getAmount_CorrectValue_RegularMovie() {
-    Rental rental = new Rental(MOVIE_REGULAR, 1); // simplest case, 1 day rented
-    assertEquals(2, rental.getRentalPrice());
+  @ParameterizedTest(name = "{index} => days={0}, cost={1}")
+  @CsvSource({"1,2", "2,2", "7,9.5"})
+  void getAmount_CorrectValue_RegularMovie(int days, double cost) {
+    Rental rental = new Rental(MOVIE_REGULAR, days); // simplest case, 1 day rented
+    assertEquals(cost, rental.getRentalPrice());
   }
 
-  @Test
-  void getAmount_CorrectValue_ChildrensMovie() {
-    Rental rental = new Rental(MOVIE_CHILDRENS, 1); // simplest case, 1 day rented
-    assertEquals(1.5, rental.getRentalPrice());
+  @ParameterizedTest(name = "{index} => days={0}, cost={1}")
+  @CsvSource({"1,1.5", "2,1.5", "7,7.5"})
+  void getAmount_CorrectValue_ChildrensMovie(int days, double cost) {
+    Rental rental = new Rental(MOVIE_CHILDRENS, days); // simplest case, 1 day rented
+    assertEquals(cost, rental.getRentalPrice());
   }
 
 }
