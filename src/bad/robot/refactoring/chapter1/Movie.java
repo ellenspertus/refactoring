0d3c011af6b9a71618bcc9d356d823dd2f059b75
OBNA <obn.adeleke@gmail.com>
2020-11-13 04:38:14 -0600

    Extract getFrequentRenterPoints as method place new extracted method and update class and method calls

--- src/bad/robot/refactoring/chapter1/Movie.java
+++ src/bad/robot/refactoring/chapter1/Movie.java
@@ -2,28 +2,37 @@ package bad.robot.refactoring.chapter1;
 
 public class Movie {
 
-    public static final int CHILDREN = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
+	public static final int CHILDREN = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
 
-    private String title;
-    private int priceCode;
+	private String title;
+	private int priceCode;
 
-    public Movie(String title, int priceCode) {
-        this.title = title;
-        this.priceCode = priceCode;
-    }
+	public Movie(String title, int priceCode) {
+		this.title = title;
+		this.priceCode = priceCode;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public int getPriceCode() {
-        return priceCode;
-    }
+	public int getPriceCode() {
+		return priceCode;
+	}
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
-    }
+	public void setPriceCode(int priceCode) {
+		this.priceCode = priceCode;
+	}
+
+	protected int getFrequentRenterPoints(int daysRented) {
+		int frequentRenterPoints = 1;
+		// add bonus for a two day new release rental
+		if (priceCode == NEW_RELEASE && daysRented > 1) {
+			frequentRenterPoints++;
+		}
+		return frequentRenterPoints;
+	}
 
 }
