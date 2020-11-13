a14920ff7df2ced18c08c1f71f3009d104dad238
OBNA <obn.adeleke@gmail.com>
2020-11-13 02:19:48 -0600

    Refactor getRentalCost() to Rental

---
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -0,0 +1,16 @@
+package bad.robot.refactoring.chapter1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RentalTest {
+
+	private static final Movie MOVIE_NEW = new Movie("New Movie", Movie.NEW_RELEASE);
+	private static final Movie MOVIE_CHILDRENS = new Movie("Children's Movie", Movie.CHILDREN);
+	private static final Movie MOVIE_REGULAR = new Movie("Regular Movie", Movie.REGULAR);
+
+
+	
+
+}
