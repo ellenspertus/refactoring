02fa6b949ee0c930664fef6c1d6ed9e968a8c564
Sarah O'Reilly <soreilly@mills.edu>
2020-12-08 19:26:50 -0800

    add .doubleValue() to BigDecimal tests

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -14,40 +14,42 @@ class RentalTest {
     @Test
     void getRentalPrice_CorrectValue_RegularMovieTwoDay() {
         Rental regTwoDay = new Rental(MOVIE_REGULAR, 2);
-        assertEquals(BigDecimal.valueOf(2), regTwoDay.getRentalPrice());
+        assertEquals(BigDecimal.valueOf(2).doubleValue(), regTwoDay.getRentalPrice().doubleValue());
     }
 
     @ParameterizedTest
     @CsvSource(value = {"1, 1.5", "3, 1.5", "4, 1.5"})
     void getRentalPrice_CorrectValue_ChildrensMovieSO(int days, BigDecimal price) {
         Rental childRental = new Rental(MOVIE_CHILDRENS, 3);
-        assertEquals(price, childRental.getRentalPrice(), "Wrong cost for " + days + "days.");
+        assertEquals(price.doubleValue(), childRental.getRentalPrice().doubleValue(),
+                "Wrong cost for " + days + "days.");
     }
 
     @ParameterizedTest
-    @CsvSource({"1,1.5", "2,1.5", "3,1.5", "5,4.5", "6,6.0", "9999,14995.5"})
+    @CsvSource({"1,1.5", "2,1.5", "3,1.5", "5,4.5", "6,6", "9999,14995.5"})
     void getAmount_CorrectValue_ChildrensMovie(int days, BigDecimal price) {
         Rental rental = new Rental(MOVIE_CHILDRENS, days);
-        assertEquals(price, rental.getRentalPrice());
+        assertEquals(price.doubleValue(), rental.getRentalPrice().doubleValue());
     }
 
     @Test
     void getAmount_CorrectValue_RegularMovie() {
         Rental rental = new Rental(MOVIE_REGULAR, 1);
-        assertEquals(BigDecimal.valueOf(2), rental.getRentalPrice());
+        assertEquals(BigDecimal.valueOf(2).doubleValue(), rental.getRentalPrice().doubleValue());
     }
 
     @ParameterizedTest
-    @CsvSource({"3,3.5", "4,5.0", "1,2", "5,6.5"})
+    @CsvSource({"3,3.5", "4,5", "1,2", "5,6.5"})
     void getAmount_CorrectValue_RegularMovies(int daysRented, BigDecimal expectedPrice) {
         Rental rental = new Rental(MOVIE_REGULAR, daysRented);
-        assertEquals(expectedPrice, rental.getRentalPrice());
+        assertEquals(expectedPrice.doubleValue(), rental.getRentalPrice().doubleValue());
     }
 
     @ParameterizedTest
     @CsvSource({"1,3", "2,6", "3,9", "10,30"})
     void getAmount_CorrectValue_NewMovie(int days, BigDecimal cost) {
         Rental rental = new Rental(MOVIE_NEW, days);
-        assertEquals(cost, rental.getRentalPrice(), "Wrong cost for " + days + "-day rental");
+        assertEquals(cost.doubleValue(), rental.getRentalPrice().doubleValue(),
+                "Wrong cost for " + days + "-day rental");
     }
 }
