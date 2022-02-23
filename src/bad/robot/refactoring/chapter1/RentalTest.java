d794faee5239c3de99b0b745b1368a7921ba01bd
Hannele <hjane@mills.edu>
2022-02-22 16:40:59 -0800

    Add unit tests

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -11,10 +11,17 @@ class RentalTest {
     private static final Movie THE_BLIND_SIDE = new Movie("The Blind Side", Movie.REGULAR);
 
     @ParameterizedTest
+
     @CsvSource(value = {"3, 1.5", "5, 4.5", "10, 12"})
     void testGetChildrenPrice(int days, double expected) {
         Rental childrenMovie = new Rental(FINDING_NEMO, days);
         assertEquals(expected, childrenMovie.getPrice());
+    }
+
+    @CsvSource({"1, 2", "3, 3.5", "10, 14"})
+    void testGetPrice(int days, double expected) {
+        Rental rentTest = new Rental(THE_BLIND_SIDE, days);
+        assertEquals(expected, rentTest.getPrice());
     }
 
     @CsvSource(value = {"2, 6", "4, 12", "1,3"})
