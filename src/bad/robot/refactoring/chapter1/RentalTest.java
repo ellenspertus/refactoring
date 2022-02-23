b89cffe64bfc2f1543e7c97ce89e782869ef8a6d
Vienna Tran <vitran@mills.edu>
2022-02-22 16:50:37 -0800

    add unit tests for get price for children's movie

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,5 +1,6 @@
 package bad.robot.refactoring.chapter1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.*;
 
@@ -11,7 +12,20 @@ class RentalTest {
 
     @Test
     void testGetPrice() {
-        fail("Not yet implemented");
+        Rental m1 = new Rental (FINDING_NEMO, 3);
+        assertEquals(1.5,m1.getPrice());
+    }
+
+    @Test
+    void testGetPrice2() {
+        Rental m1 = new Rental (FINDING_NEMO, 5);
+        assertEquals(4.5,m1.getPrice());
+    }
+
+    @Test
+    void testGetPrice3() {
+        Rental m1 = new Rental (FINDING_NEMO, 10);
+        assertEquals(12,m1.getPrice());
     }
 
 }
