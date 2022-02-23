340856ee995a1dc754fb0ababb9cb34feeee44be
Hannele <hjane@mills.edu>
2022-02-22 16:40:59 -0800

    Add unit tests

--- src/bad/robot/refactoring/chapter1/RentalTest.java
+++ src/bad/robot/refactoring/chapter1/RentalTest.java
@@ -1,7 +1,8 @@
 package bad.robot.refactoring.chapter1;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
 
 class RentalTest {
     private static final Movie FINDING_NEMO = new Movie("Finding Nemo", Movie.CHILDREN);
@@ -9,9 +10,11 @@ class RentalTest {
             new Movie("Death on the Nile", Movie.NEW_RELEASE);
     private static final Movie THE_BLIND_SIDE = new Movie("The Blind Side", Movie.REGULAR);
 
-    @Test
-    void testGetPrice() {
-        fail("Not yet implemented");
+    @ParameterizedTest
+    @CsvSource({"1, 2", "3, 3.5", "10, 14"})
+    void testGetPrice(int days, double expected) {
+        Rental rentTest = new Rental(THE_BLIND_SIDE, days);
+        assertEquals(expected, rentTest.getPrice());
     }
 
 }
