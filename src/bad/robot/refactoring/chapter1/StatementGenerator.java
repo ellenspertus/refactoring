e2e5038a144a25af2268c0dfead2fea2f2a20f08
Ellen Spertus <spertus@mills.edu>
2022-03-01 15:46:09 -0800

    Make StatementGenerator abstract

--- src/bad/robot/refactoring/chapter1/StatementGenerator.java
+++ src/bad/robot/refactoring/chapter1/StatementGenerator.java
@@ -3,36 +3,40 @@ package bad.robot.refactoring.chapter1;
 import java.math.*;
 import java.text.*;
 
-public class StatementGenerator {
-    private static final String HEADER_TEMPLATE = "Rental record for %1s\n";
-    private static final String RENTAL_TEMPLATE = "\t%1s\t%2s\n";
-    private static final String FOOTER_TEMPLATE =
-            "Amount owed is %1s\nYou earned %2s frequent renter points";
+public abstract class StatementGenerator {
 
-    public static String generateStatement(Customer customer) {
+    private final String headerTemplate, rentalTemplate, footerTemplate;
+
+    protected StatementGenerator(String header, String rental, String footer) {
+        headerTemplate = header;
+        rentalTemplate = rental;
+        footerTemplate = footer;
+    }
+
+    public String generateStatement(Customer customer) {
         String result = generateHeader(customer);
         result += generateBody(customer);
         result += generateFooter(customer);
         return result;
     }
 
-    private static String generateHeader(Customer customer) {
-        return String.format(HEADER_TEMPLATE, customer.getName());
+    private String generateHeader(Customer customer) {
+        return String.format(headerTemplate, customer.getName());
     }
 
-    private static String generateBody(Customer customer) {
+    private String generateBody(Customer customer) {
         StringBuilder result = new StringBuilder();
         for (Rental rental : customer.getRentals()) {
-            result.append(String.format(RENTAL_TEMPLATE, rental.getMovie().getTitle(),
+            result.append(String.format(rentalTemplate, rental.getMovie().getTitle(),
                     bigDecimalToCurrency(rental.getPrice())));
         }
         return result.toString();
     }
 
-    private static String generateFooter(Customer customer) {
+    private String generateFooter(Customer customer) {
         int frequentRenterPoints = customer.calculateFrequentRenterPoints();
         BigDecimal totalAmount = customer.calculateTotalPrice();
-        return String.format(FOOTER_TEMPLATE, bigDecimalToCurrency(totalAmount),
+        return String.format(footerTemplate, bigDecimalToCurrency(totalAmount),
                 frequentRenterPoints);
     }
 
