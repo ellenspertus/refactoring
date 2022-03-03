452ef91ee1fd63d5600d59878c929d4b4273141e
Ellen Spertus <spertus@mills.edu>
2022-03-03 15:05:31 -0800

    Make SpanishStatementGenerator singleton

--- src/bad/robot/refactoring/chapter1/SpanishStatementGenerator.java
+++ src/bad/robot/refactoring/chapter1/SpanishStatementGenerator.java
@@ -5,9 +5,13 @@ public class SpanishStatementGenerator extends StatementGenerator {
     private static final String RENTAL_TEMPLATE = "\t%1s\t%2s\n";
     private static final String FOOTER_TEMPLATE =
             "El monto adeudado es %1s\nGanaste %2s puntos de arrendatario frecuente";
+    private static final SpanishStatementGenerator INSTANCE = new SpanishStatementGenerator();
 
-    public SpanishStatementGenerator() {
+    private SpanishStatementGenerator() {
         super(HEADER_TEMPLATE, RENTAL_TEMPLATE, FOOTER_TEMPLATE);
     }
 
+    public static SpanishStatementGenerator getInstance() {
+        return INSTANCE;
+    }
 }
