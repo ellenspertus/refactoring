19e78cf89d09ec9e9fcf808f6a6031ce9f9d9a60
Ellen Spertus <spertus@mills.edu>
2022-03-03 15:33:41 -0800

    Use state and singleton pattern for price, points

--- src/bad/robot/refactoring/chapter1/Rental.java
+++ src/bad/robot/refactoring/chapter1/Rental.java
@@ -21,38 +21,11 @@ public class Rental {
     }
 
     public BigDecimal getPrice() {
-        BigDecimal amount = BigDecimal.ZERO;
-
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount = BigDecimal.valueOf(2);
-                if (getDaysRented() > 2) {
-                    amount = amount.add(new BigDecimal("1.5")
-                            .multiply(BigDecimal.valueOf(getDaysRented() - 2)));
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                amount = BigDecimal.valueOf(getDaysRented() * 3);
-                break;
-            case Movie.CHILDREN:
-                amount = new BigDecimal("1.5");
-                if (getDaysRented() > 3) {
-                    amount = amount.add(new BigDecimal("1.5")
-                            .multiply(BigDecimal.valueOf(getDaysRented() - 3)));
-                }
-                break;
-        }
-        return amount;
+        Price price = getMovie().getPrice(); // either RegularPrice, ChildrensPrice, etc.
+        return price.getCharge(getDaysRented());
     }
 
     public int calculateFrequentRenterPoints() {
-        int frequentRenterPoints = 1;
-
-        // add bonus for a two day new release rental
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-            frequentRenterPoints++;
-        }
-
-        return frequentRenterPoints;
+        return getMovie().getPrice().getFrequentRenterPoints(getDaysRented());
     }
 }
